40b9992e3a61b3fc997a494c069741ee0f05f823
poncel1968 <40808687+poncel1968@users.noreply.github.com>
2018-11-15 21:36:35 -0300

    Add files via upload

---
+++ EjercicioPre2doParcialV3/Compra.h
@@ -0,0 +1,41 @@
+#ifndef COMPRA_H_INCLUDED
+#define COMPRA_H_INCLUDED
+
+typedef struct
+{
+  char nombreCliente[128];
+  int idProducto;
+  int unidades;
+  float precioUnitario;
+  float iva;
+  float montoTotal;
+}Compra;
+
+void com_calcularMonto(void* p);
+
+#endif // COMPRA_H_INCLUDED
+
+
+Compra* Compra_new();
+void Compra_delete();
+Compra* Compra_newConParametros(char* nombreCliente,int idProducto,int unidades,float precioUnitario,float iva,float montoTotal);
+
+int Compra_setNombreCliente(Compra* this,char* nombreCliente);
+int Compra_getNombreCliente(Compra* this,char* nombreCliente);
+
+int Compra_setIdProducto(Compra* this,int idProducto);
+int Compra_getIdProducto(Compra* this,int* idProducto);
+
+int Compra_setUnidades(Compra* this,int unidades);
+int Compra_getUnidades(Compra* this,int* unidades);
+
+int Compra_setPrecioUnitario(Compra* this,float precioUnitario);
+int Compra_getPrecioUnitario(Compra* this,float* precioUnitario);
+
+int Compra_setIva(Compra* this,float iva);
+int Compra_getIva(Compra* this,float* iva);
+
+int Compra_setMontoTotal(Compra* this,float montoTotal);
+int Compra_getMontoTotal(Compra* this,float* montoTotal);
+
+#endif // COMPRA_H_INCLUDED
---
+++ EjercicioPre2doParcialV3/Parser.c
@@ -0,0 +1,58 @@
+#include "LinkedList.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int parser_parseCompras(char* fileName, LinkedList* lista)
+{
+  char nombreCliente[128];
+  char* idProductoStr;
+  int idProducto;
+  char* unidadesStr;
+  int unidades;
+  char* precioUnitarioStr;
+  float precioUnitario;
+  char* ivaPorcentajeStr;
+  float ivaPorcentaje;
+  float montoTotal;
+  FILE* pFile = fopen(fileName,"r");
+  ///nombreCliente,idProducto,precioUnitario,unidades,iva
+
+    int retorno=-1;
+    int flagOnce=1;
+    Compra * pCompra;
+
+    if(pFile!=NULL)
+    {
+        while(!feof(pFile))
+        {
+            if( flagOnce)
+            {
+                flagOnce=0;
+                fscanf(pFile,"%[^,],%[^,],%[^,],%[^,],%[^\n]\n",nombreCliente,idProductoStr,precioUnitarioStr,unidadesStr,ivaPorcentajeStr);
+            }
+            fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n",bufferInt,bufferNombre,bufferHorasTrabajadas,bufferSueldo);
+
+            idProducto=atoi(idProductoStr);
+            unidades=atoi(unidadesStr);
+            precioUnitario=atof(precioUnitarioStr);
+            ivaPorcentaje=atof(ivaPorcentajeStr);
+            montoTotal=precioUnitario*unidades;
+
+            pCompra = Compra_newConParametros(nombreCliente,idProducto,unidades,precioUnitario,iva,montoTotal);
+
+            if(Compra!=NULL)
+            {
+                retorno=0;
+                ll_add(lista,Compra);
+            }
+
+        }
+
+    }
+
+    return retorno;
+
+  ///  return 1;  OK
+}
+
